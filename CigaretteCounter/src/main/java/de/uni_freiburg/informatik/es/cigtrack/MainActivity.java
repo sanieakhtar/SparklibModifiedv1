b5127b3bd84c5197caa8c2c7be90c51cbb2d7e5c
sanieakhtar <sanieakhtar@yahoo.com>
2017-06-29 10:45:44 +0200

    Initial commit v1

---
+++ CigaretteCounter/src/main/java/de/uni_freiburg/informatik/es/cigtrack/MainActivity.java
@@ -0,0 +1,123 @@
+package de.uni_freiburg.informatik.es.cigtrack;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import eu.senseable.sparklib.Spark;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Spark mSpark = null;
+    public Spark.Callbacks mSparkCalls = new Spark.Callbacks.Stub() {
+        @Override
+        public void onEventsChanged(List<Spark.Event> events) {
+            super.onEventsChanged(events);
+
+            int numcigs = events.size();
+            String msg = getResources().getQuantityString(R.plurals.numcigs, numcigs, numcigs);
+            TextView txt = (TextView) findViewById(R.id.text);
+            txt.setText(msg);
+        }
+    };
+
+    private View.OnClickListener mRemoveEventAction = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                List<Spark.Event> evs = mSpark.getEvents();
+                if (evs.size() == 0)
+                    return;
+
+                evs.remove(evs.size() - 1);
+                mSpark.setEvents(evs);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private View.OnClickListener mAddEventAction = new View.OnClickListener() {
+        @Override
+        public void onClick(View b) {
+            try {
+                List<Spark.Event> evs = mSpark.getEvents();
+
+
+                int numcigs = evs.size();
+                //Spark.Event e = evs.get(0);
+                //evs.add(numcigs,e);
+                //mSpark.setEvents(evs);
+
+                ////////////////////////////////////////////////////////////////////////////////////
+                final Context context = getApplicationContext();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date();
+                String time = sdf.format(date);
+                Spark.Event ee = new Spark(context, mSparkCalls).new Event(time);
+                evs.add(numcigs,ee);
+                mSpark.setEvents(evs);
+
+                ////////////////////////////////////////////////////////////////////////////////////
+
+                int numcigsnew = evs.size();
+                String msg = getResources().getQuantityString(R.plurals.numcigs, numcigsnew, numcigsnew);
+                TextView txt = (TextView) findViewById(R.id.text);
+                txt.setText(msg);
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        };
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(mRemoveEventAction);
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(mAddEventAction);
+
+        mSpark = new Spark(this, mSparkCalls);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
